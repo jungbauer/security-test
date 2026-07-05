cf6b7dc068a6fdd2ccf4e54c0d34541e393cff4d
Johann Jungbauer <jungbauer@gmail.com>
2026-07-05 14:37:56 +0200

    feat: adds getAuthorities to CustomUserDetailsService

--- src/main/java/com/jungbauer/securitytest/service/CustomUserDetailsService.java
+++ src/main/java/com/jungbauer/securitytest/service/CustomUserDetailsService.java
@@ -1,15 +1,18 @@
 package com.jungbauer.securitytest.service;
 
+import com.jungbauer.securitytest.model.entity.Role;
 import com.jungbauer.securitytest.model.entity.TestUser;
 import com.jungbauer.securitytest.repository.TestUserRepository;
 import org.jspecify.annotations.NonNull;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 @Service
@@ -29,15 +32,23 @@ public class CustomUserDetailsService implements UserDetailsService {
             throw new UsernameNotFoundException("No user found with email: " + email);
         }
 
-        // todo remove when these are in user entity
+        // These are required by UserDetails. This could be implemented in TestUser, but I don't want to.
         boolean enabled = true;
         boolean accountNonExpired = true;
         boolean credentialsNonExpired = true;
         boolean accountNonLocked = true;
-        List<GrantedAuthority> authorities = new ArrayList<>();
+        List<GrantedAuthority> authorities = getAuthorities(user.getRoles());
 
         return new org.springframework.security.core.userdetails.User(
                 user.getEmail(), user.getPassword(), enabled, accountNonExpired,
                 credentialsNonExpired, accountNonLocked, authorities);
+    }
+
+    private List<GrantedAuthority> getAuthorities (Collection<Role> roles) {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        for (Role role : roles) {
+            authorities.add(new SimpleGrantedAuthority(role.getName().name()));
+        }
+        return authorities;
     }
 }
