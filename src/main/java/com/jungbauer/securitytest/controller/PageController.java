aae8ae08d57d40ac02a3ac35d1dc1322fbfb9d7a
Johann Jungbauer <jungbauer@gmail.com>
2026-07-06 13:37:52 +0200

    feat: adds method role security for some pages

--- src/main/java/com/jungbauer/securitytest/controller/PageController.java
+++ src/main/java/com/jungbauer/securitytest/controller/PageController.java
@@ -1,5 +1,6 @@
 package com.jungbauer.securitytest.controller;
 
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -14,5 +15,29 @@ public class PageController {
     @GetMapping("/test")
     public String testPage() {
         return "test";
+    }
+
+    @GetMapping("/test2")
+    @PreAuthorize("isAuthenticated()")
+    public String test2Page() {
+        return "test2";
+    }
+
+    @GetMapping("/user")
+    @PreAuthorize("isAuthenticated()")
+    public String userPage() {
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    @PreAuthorize("hasAnyRole('ROLE_ADMIN', 'ROLE_OWNER')")
+    public String adminPage() {
+        return "admin";
+    }
+
+    @GetMapping("/owner")
+    @PreAuthorize("hasRole('OWNER')")
+    public String ownerPage() {
+        return "owner";
     }
 }
